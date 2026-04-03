2aa50c3f04c1a7b5741eaac7d343bc0ff0b9739d
SoftwarEnTalla <softwarentalla@gmail.com>
2026-04-02 17:31:51 -0700

    Initial commit

--- src/common/types/logger.type.ts
+++
@@ -1,6 +0,0 @@
-import { HttpLoggerApiRest, ILoggerClient } from "src/interfaces/log-context";
-
-export type LoggerCallback = (
-  logData: HttpLoggerApiRest,
-  client: ILoggerClient
-) => Promise<boolean>;
--- src/core/adapters/transloco-adapter.js
+++
@@ -1,4 +0,0 @@
-// src/core/adapters/transloco-adapter.js
-const esmRequire = require('esm')(module);
-
-module.exports = esmRequire('@jsverse/transloco');
