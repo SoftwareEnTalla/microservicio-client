503b529b51fd17ab939331c20e8d570892521bfa
SoftwarEnTalla <softwarentalla@gmail.com>
2026-04-04 17:32:22 -0700

    Actualizar configuración inicial del submódulo client-service

--- src/config/swagger-config.ts
+++ src/config/swagger-config.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026 SoftwarEnTalla
+ * Copyright (c) 2025 SoftwarEnTalla
  * Licencia: MIT
  * Contacto: softwarentalla@gmail.com
  * CEOs: 
@@ -29,10 +29,7 @@
  */
 
 
-import { ClientModule } from "@modules/client/modules/client.module";
-import { ClientLoyaltyTierModule } from "@modules/client-loyalty-tier/modules/clientloyaltytier.module";
-import { ClientSegmentModule } from "@modules/client-segment/modules/clientsegment.module";
-import { ClientTypeModule } from "@modules/client-type/modules/clienttype.module";
+import { CodetraceModule } from "@modules/codetrace/modules/codetrace.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { logger } from '@core/logs/logger';
 
@@ -41,8 +38,8 @@ import { logger } from '@core/logs/logger';
 export function setupSwagger(
   app,
   apiDoc: string = "api-docs",
-  title: string = "Client Service API",
-  description: string = "API completa para gestión de Clients con documentación automática",
+  title: string = "Codetrace Service API",
+  description: string = "API completa para gestión de Codetraces con documentación automática",
   version: string = "1.0"
 ): string {
 try{
@@ -52,7 +49,7 @@ try{
         .setVersion(version)
         // Organiza por módulos/funcionalidades
         //.addTag("Authentication", "Operaciones de autenticación y usuarios")
-        //.addTag("Clients", "Gestión de transacciones y procesamiento de clients")
+        //.addTag("Codetraces", "Gestión de transacciones y procesamiento de codetraces")
         //.addTag("Subscriptions", "Manejo de suscripciones recurrentes")
         //.addTag("Webhooks", "Endpoints para integraciones externas")
         //.addTag("Reports", "Generación de reportes y analytics")
@@ -75,9 +72,7 @@ try{
         .build();
 
       const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig, {
-        include: [ClientModule,         ClientLoyaltyTierModule,
-        ClientSegmentModule,
-        ClientTypeModule,/*, AuthModule, ReportsModule*/], // Lista todos los módulos
+        include: [CodetraceModule /*, AuthModule, ReportsModule*/], // Lista todos los módulos
         deepScanRoutes: true, // Escanea en profundidad
         ignoreGlobalPrefix: false, // Considera el prefijo global (api/)
         extraModels: [], // Añade esto
@@ -99,7 +94,7 @@ try{
           displayRequestDuration: true,
         },
         customCss: ".swagger-ui .topbar { background-color: #2c3e50; }", // Personalización
-        customSiteTitle: "Client API Docs",
+        customSiteTitle: "Codetrace API Docs",
         customfavIcon: "/favicon.ico",
       });
   }
